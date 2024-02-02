dfbf96690176a96e208d1504b1b1adbe21774910
roy5820 <troy5830@gmail.com>
2024-02-02 18:27:09 +0900

    애니메이션 관리하는 컴포넌트 구현

--- Assets/Script/Character/CharacterAnimationManager.cs
+++ Assets/Script/Character/CharacterAnimationManager.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterAnimationManager : MonoBehaviour
+public class CharacterAnimationManager : MonoBehaviour, AnimationInterface
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator thisAnimator;//해당 오브젝트의 애니메이션 컴포넌트
 
-    // Update is called once per frame
-    void Update()
+    //지정한 값으로 NowAniNum을 변경하여 해당 번호값에 해당하는 애니메이션 재생
+    public void SetAniParameter(int value)
     {
-        
+        if (thisAnimator == null) return;//애니메이션 컴포넌트가 없으면
+        thisAnimator.SetInteger("NowAniNum", value);
     }
 }
